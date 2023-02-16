4eb41c849f7e83db21897111be94054bf403d43d
kotharamya <kotharamya99@gmail.com>
2023-02-15 20:47:48 -0800

    added labels and menu pages in frontend

---
+++ label_frontend/src/components/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Menu() {
+  return (
+    <div className="menu">
+      <ul>
+        <li>
+          Actions
+          <ul>
+            <li>Edit Labels</li>
+            <li>Import Labels</li>
+          </ul>
+        </li>
+        <li>In-Actions</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
