ef062141ad41590284b19e7e0541d0e8ed6dc379
kotharamya <kotharamya99@gmail.com>
2023-04-10 12:08:26 -0700

    added changes in the models

---
+++ backend/backend/backend/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='./')
+
+class Labels(models.Model):
+    name = models.CharField(max_length=200)
+    id = models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return self.name
